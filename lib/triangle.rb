b5d0ef2e62461e3be407cad4194424badd76e23e
Matthew McCarthy <mmccarthy64@hotmail.com>
2020-09-04 21:33:58 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -4,8 +4,7 @@ class Triangle
   def initialize(side1, side2, side3)
     if side3 = side2 = side1
       :equilateral
-    elsif
-      
+    elsif side1 = side2 != side3 || side1 != side2 = side3 || side1 = side3 != side2
     
   end
   
