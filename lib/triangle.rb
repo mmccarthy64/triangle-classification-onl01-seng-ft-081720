6f8ce7013aa3f7921c43932ac3828a764759ac91
Matthew McCarthy <mmccarthy64@hotmail.com>
2020-09-04 21:46:40 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,6 +10,9 @@ class Triangle
   end
   
   def kind
+    if (@side1 < 0) || (side2 < 0) || (@side3 < 0)
+      raise TriangleError
+    elsif 
     
     if side3 = side2 = side1
       :equilateral
