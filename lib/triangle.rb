b941cff577cd975404080cdab39b4533a9e4a1c7
Matthew McCarthy <mmccarthy64@hotmail.com>
2020-09-04 21:41:46 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,6 +2,10 @@ class Triangle
   # write code here
   
   def initialize(side1, side2, side3)
+
+  end
+  
+  def kind
     if side1, side2, side3 < 0
       begin
         raise TriangleError
@@ -10,16 +14,14 @@ class Triangle
       end
       
     end
-  end
-  
-  def kind
-      if side3 = side2 = side1
-        :equilateral
-      elsif side1 = side2 != side3 || side1 != side2 = side3 || side1 = side3 != side2
-        :isosceles
-      elsif side1 != side2 != side3
-        :scalene
-      end
+    
+    if side3 = side2 = side1
+      :equilateral
+    elsif side1 = side2 != side3 || side1 != side2 = side3 || side1 = side3 != side2
+      :isosceles
+    elsif side1 != side2 != side3
+      :scalene
+    end
   end
   
   class TriangleError < StandardError
